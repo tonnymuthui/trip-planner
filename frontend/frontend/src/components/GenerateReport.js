afab295dde16103a868830d00d42c2788353b189
tonnymuthui <69537407+tonnymuthui@users.noreply.github.com>
2025-04-02 16:57:07 +0300

    Fix report endpoint

--- frontend/frontend/src/components/GenerateReport.js
+++ frontend/frontend/src/components/GenerateReport.js
@@ -42,7 +42,7 @@ const GenerateReport = ({ selectedTripId }) => {
       const tripResponse = await axios.get(`https://trip-planner-1-x88j.onrender.com/api/trips/${id}/`,
           { headers: { Authorization: `Bearer ${token}` } }
       );
-      const logResponse = await axios.get(`https://trip-planner-1-x88j.onrender.com/api/logentries/?trip_id=${id}/`,
+      const logResponse = await axios.get(`https://trip-planner-1-x88j.onrender.com/api/trips/${id}/`,
         { headers: { Authorization: `Bearer ${token}` } }
       );
 
