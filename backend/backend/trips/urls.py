16a4eb3949c9386cbb469d81848a09e478793b9c
tonnymuthui <69537407+tonnymuthui@users.noreply.github.com>
2025-04-02 15:01:45 +0300

    Fixed report endpoint

--- backend/backend/trips/urls.py
+++ backend/backend/trips/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path("geocode/", geocode_location, name="geocode"),
     
-    path('trips/<str:trip_id>/report/', TripReportView.as_view(), name='trip_report'), 
+    path('trip-report/<str:trip_id>/report/', TripReportView.as_view(), name='trip_report'), 
     path('trip-logs/<str:trip_id>/logs/', TripLogsView.as_view(), name='trip_logs'),
     path('user/', UserInfoView.as_view(), name='user_info'),
     path('compliance-summary/', ComplianceSummaryView.as_view(), name='compliance_summary')
